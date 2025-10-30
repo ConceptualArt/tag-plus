a4e0266f5bde8652dafac96bf675da361437eaeb
Jackson Fernando Merma Portocarrero <jmermap@unsa.edu.pe>
2025-10-30 01:50:24 -0500

    Update play/pause button icons and header title for clarity

--- popup.js
+++ popup.js
@@ -69,14 +69,14 @@ document.addEventListener('DOMContentLoaded', function () {
             playPauseBtn.style.backgroundColor = '#6B4C29'; // Brown when active
             playPauseBtn.style.color = '#FFFFFF';
             playPauseBtn.title = 'Highlighting ON - Click to disable';
-            // Change to play icon (complete SVG replacement)
-            playPauseBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-fill" viewBox="0 0 16 16" id="toggle-icon"><path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393"/></svg>';
+            // Change to PAUSE icon when highlighting is active
+            playPauseBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pause-fill" viewBox="0 0 16 16" id="toggle-icon"><path d="M5.5 3.5A1.5 1.5 0 0 1 7 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5m5 0A1.5 1.5 0 0 1 12 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5"/></svg>';
         } else {
             playPauseBtn.style.backgroundColor = '';
             playPauseBtn.style.color = '';
             playPauseBtn.title = 'Highlighting OFF - Click to enable';
-            // Change to pause icon (complete SVG replacement)
-            playPauseBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pause-fill" viewBox="0 0 16 16" id="toggle-icon"><path d="M5.5 3.5A1.5 1.5 0 0 1 7 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5m5 0A1.5 1.5 0 0 1 12 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5"/></svg>';
+            // Change to PLAY icon when highlighting is inactive
+            playPauseBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-fill" viewBox="0 0 16 16" id="toggle-icon"><path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393"/></svg>';
         }
     }
 
